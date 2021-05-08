9d8c0231c6aa885846672584d377bfb1c1bffe0c
Matheus KBank <matheus8476@outlook.com>
2021-05-08 10:29:53 -0300

    conclusao do app ejs

--- nodejs-ejs-html/server.js
+++ nodejs-ejs-html/server.js
@@ -1,15 +1,44 @@
 const express = require('express');
 
 const app = express();
-
+app.use(express.static('./public'));
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
-    res.render('index');
+app.get('/', (req, res) => {
+    const items = [
+        {
+            title: "D",
+            message: 'Desenvolver com Node'
+        },
+        {
+            title: "E",
+            message: 'EJs usa JS'
+        },
+        {
+            title: "M",
+            message: 'Palavra com M'
+        },
+        {
+            title: "A",
+            message: 'Palavra com A'
+        },
+        {
+            title: "I",
+            message: 'Palavra com I'
+        },
+        {
+            title: "S",
+            message: 'Palavra com S'
+        }        
+    ];
+
+    res.render('pages/index', {
+        qualitys: items,
+    });
 });
 
 app.get('/sobre', function(req, res) {
-    res.render('about');
+    res.render('pages/about');
 });
 app.listen(8080);
 console.log('rodando');
